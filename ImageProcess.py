004d1d947b81631975d3ccc56b648573919f919b
Shyamal Ruparel <shy@shyruparel.com>
2012-12-01 16:49:23 -0500

    Fixed some minor tabbing issues.

--- ImageProcess.py
+++ ImageProcess.py
@@ -7,51 +7,51 @@ start = '/home/thelonelygod/Documents/shiny-dangerzone/Start.jpg'
 name =  '/home/thelonelygod/Documents/shiny-dangerzone/img1.jpg'
 
 class Capture:
-	def getImage1(self,name):	
-		pygame.image.save(image,name)
-		img = Image.open(name)
+        def getImage1(self,name):       
+                pygame.image.save(image,name)
+                img = Image.open(name)
 
-		return img
+                return img
 
-	def compare(self,img1,img2):		
-		img = ImageChops.difference(img1,img2)
-		xsize , ysize = img.size
+        def compare(self,img1,img2):            
+                img = ImageChops.difference(img1,img2)
+                xsize , ysize = img.size
 
-		for s in range(0,xsize/3):
-			s=s*3
-			for m in range (0,ysize/3):
-				m=m*3
-				r,g,b = img.getpixel((s,m))	
-				if g > 150:
-					return True
+                for s in range(0,xsize/3):
+                        s=s*3
+                        for m in range (0,ysize/3):
+                                m=m*3
+                                r,g,b = img.getpixel((s,m))     
+                                if g > 150:
+                                        return True
 
-		return False		
-	
-	def main(self):
-		pygame.init()
-		pygame.camera.init()
+                return False            
+        
+        def main(self):
+                pygame.init()
+                pygame.camera.init()
 
-		cam = pygame.camera.Camera("/dev/video0",(160,120))
+                cam = pygame.camera.Camera("/dev/video0",(160,120))
 
-		cam.start()
+                cam.start()
 
-		image = cam.get_image()
-		pygame.image.save(image,start)
-		cam.stop()
+                image = cam.get_image()
+                pygame.image.save(image,start)
+                cam.stop()
         def switch(self):
                 start = name
-	def mainComp(self):
-		pygame.init()
-		pygame.camera.init()
+        def mainComp(self):
+                pygame.init()
+                pygame.camera.init()
 
-		cam = pygame.camera.Camera("/dev/video0",(160,120))
+                cam = pygame.camera.Camera("/dev/video0",(160,120))
 
-		cam.start()
+                cam.start()
 
-		image = cam.get_image()
-		pygame.image.save(image,name)
-		cam.stop()
-		
-		img1 = Image.open(start)
-		img2 = Image.open(name)
-		return compare (img1,img2)		
+                image = cam.get_image()
+                pygame.image.save(image,name)
+                cam.stop()
+                
+                img1 = Image.open(start)
+                img2 = Image.open(name)
+                return compare (img1,img2)              
